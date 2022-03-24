456e20b20ef954b0d2aa19844d8a6fcec7fef674
henryrodgers <s-rodgersh@bsd405.org>
2022-03-24 10:26:19 -0700

    customizable function.py

--- datavis/function.py
+++ datavis/function.py
@@ -16,12 +16,30 @@ X = list(df.iloc[:, 0])
 Y = list(df.iloc[:, 1])
   
 # Plot the data using bar() method
-plt.bar(X, Y, color='g')
-plt.title("Students over 11 Years")
-plt.xlabel("Years")
-plt.ylabel("Number of Students")
+cinput = input('Please enter the desired bar chart color (enter "info" for a list of possible colors): ')
+
+if (cinput == "info"):
+        print('enter "b" for blue');
+        print('enter "c" for cyan');
+        print('enter "k" for black');
+        print('enter "g" for green');
+        print('enter "m" for purple');
+        print('enter "w" for white');
+        print('enter "r" for red');
+        print('enter "y" for yellow');
+        cinput = input("Please enter the desired bar chart color: ")
+plt.bar(X, Y, color=cinput)
+dinput = input('Please enter the title: ')
+#Students over 11 Years
+plt.title(dinput)
+einput = input('Please enter the x-axis label: ')
+#Years
+plt.xlabel(einput)
+finput = input('Please enter the y-axis label: ')
+#Number of Students
+plt.ylabel(finput)
   
 # Show the plot
 plt.show()
-input()
+input('Press enter to end program.')
 sys.exit()
